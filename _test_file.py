7a8a5bb0012c032f55043edff3a83a473fe0bb46
Arshad Kazmi <arshadkazmi42@gmail.com>
2021-05-18 19:51:26 +0530

    Add file operations class and test

---
+++ _test_file.py
@@ -0,0 +1,29 @@
+import unittest
+from unittest.mock import patch, mock_open
+
+from link import Link
+from file_operations import FileOperations
+
+## TODO Figure out a way to test file read and write
+
+class TestBrokenLinkChecker(unittest.TestCase):
+
+    def test_create_file_opertaions_file_names(self):
+        URL = 'https://hackerone.com/login?xyz=123'
+        lnk = Link(URL)
+
+        file_operations = FileOperations(lnk)
+
+        self.assertEqual(file_operations.broken_file_name, 'broken.txt', f'Should return broken.txt as filename')
+        self.assertEqual(file_operations.output_file_name, 'output.txt', f'Should return output.txt as filename')
+
+    def test_create_file_operations_directory_name(self):
+        URL = 'https://hackerone.com/login?xyz=123'
+        lnk = Link(URL)
+
+        file_operations = FileOperations(lnk)
+
+        self.assertEqual(file_operations.directory_name, 'hackerone.com', f'Should return hackerone.com as directory name')
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ file_operations.py
@@ -0,0 +1,48 @@
+BROKEN_FILE_NAME = 'broken.txt'
+OUTPUT_FILE_NAME = 'output.txt'
+
+
+class FileOperations:
+
+    def __init__(self, link):
+
+        self.directory_name = link.get_domain()
+
+        self.broken_file_name = 'broken.txt'
+        self.output_file_name = 'output.txt'
+
+    def write_in_broken(self, line):
+        return self.write_line(self.broken_file_name, line)
+
+    def write_in_output(self, line):
+        return self.write_line(self.output_file_name, line)
+        
+    def write_line(self, filename, line):
+
+        if is_line_exists(file_path, line):
+            return True
+
+        f = open(filename, 'a')
+        f.write(f'{line}\n')  # python will convert \n to os.linesep
+        f.close()
+
+    def is_line_exists_in_file(self, filename, line):
+
+        if not is_file_exists(filename):
+            return False
+
+        with open(filename) as f:
+            file_content = f.read()
+            if line in file_content:
+                return True
+
+        return False
+
+    def is_file_exists(self, filename):
+
+        fyle = Path(file_path)
+
+        if fyle.is_file():
+            return True
+
+        return False
