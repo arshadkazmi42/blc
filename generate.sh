8d1ee2916ea0afca05d921aa22e23222f7ee6d69
Arshad Kazmi <arshadkazmi42@gmail.com>
2021-05-22 19:56:57 +0530

    Change generate script

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 TOOLS_PATH=$BBS_PATH/tools
 SUBDOMAINS_PATH=$BBS_PATH/subdomains
 JS_PATH=$BBS_PATH/getJS
-WBM_PATH=$BBS_PATH/wbm
+GAU_PATH=$BBS_PATH/gau
 
 if [ -f source/$1.txt ]; then
 	exit 0
@@ -9,9 +9,9 @@ fi
 
 sh $SUBDOMAINS_PATH/subdomains.sh $1
 sh $JS_PATH/run.sh $1
-sh $WBM_PATH/get.sh $1
+sh $GAU_PATH/get.sh $1
 
 # Merge JS and Live domains
-cat $SUBDOMAINS_PATH/subdomains/$1.txt $JS_PATH/results/$1"-js.txt" $WBM_PATH/output/$1.txt | sort | uniq > source/$1.txt
+cat $SUBDOMAINS_PATH/subdomains/$1.txt $JS_PATH/results/$1"-js.txt" $GAU_PATH/gau/$1.txt | sort | uniq > source/$1.txt
 
 
