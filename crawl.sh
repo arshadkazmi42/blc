b86d6e7ddb7b7b22360137e8b304bbd67fdbe9c8
Arshad Kazmi <arshadkazmi42@gmail.com>
2021-06-05 21:44:25 +0530

    Changes to run script

--- crawl.sh
+++ crawl.sh
@@ -16,13 +16,13 @@ sh $BBS_PATH/subdomains/subdomains.sh $1
 
 cat $BBS_PATH/subdomains/subdomains/$1.txt > source/$1.txt
 
-for i in $(seq 1 $DEPTH);
-do
-        cat source/$1.txt | anew crawl_old.txt > crawl_process.txt
-        
-        python3 start.py --crawl crawl_process.txt
-        
-        cat results/*/links.txt | sort | uniq > source/$1.txt
+i=1
+while [ $i -le $DEPTH ]; do
+    echo "Processing at depth: "$i
+    cat source/$1.txt | anew crawl_old.txt > crawl_process.txt
+    python3 start.py --crawl crawl_process.txt
+    cat results/*/links.txt | sort | uniq > source/$1.txt
+    i=$(($i+1))
 done
 
 
--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
-#sh crawl.sh $1
-sh generate.sh $1
+sh crawl.sh $1
+#sh generate.sh $1
 python3 start.py --file source/$1.txt
 cat results/*/broken.txt | sort | uniq > final/$1_broken.txt
 cat results/*/output.txt > final/$1_output.txt
 rm -rf results/*
+
+echo "Completed for "$1 | $BBS_PATH/tools/notify
